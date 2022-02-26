802384e95713b0c2c44190d240dee920bc2b3710
Eli-Sam <51330314+Eli-Sam@users.noreply.github.com>
2022-02-26 17:37:01 -0600

    Working on activity 6
    Steps to turn in.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,113 @@
+//declare map variable globally so all functions have access
+var map;
+var minValue;
+
+//step 1 create map
+function createMap(){
+
+    //create the map
+    map = L.map('map', {
+        center: [0, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      subdomains: 'abcd',
+      minZoom: 1,
+      maxZoom: 16,
+      ext: 'jpg'
+    }).addTo(map);
+
+    //call getData function
+    getData(map);
+};
+
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var city of data.features){
+        //loop through each year
+        for(var year = 1985; year <= 2015; year+=5){
+              //get population for current year
+              var value = city.properties["Tour_"+ String(year)];
+              //add value to array
+              allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data){
+
+    //Step 4: Determine which attribute to visualize with proportional symbols
+    var attribute = "Tour_2015";
+
+    //create marker options
+    var geojsonMarkerOptions = {
+        fillColor: "#ff7800",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        radius: 8
+    };
+
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //Step 5: For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //Step 6: Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            //create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+
+//Step 2: Import GeoJSON data
+function getData(){
+    //load the data
+    fetch("data/touristnumbers.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json);
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
+
+
+
+
 // Add all scripts to the JS folder
 /* Map of GeoJSON data from MegaCities.geojson */
 //declare map var in global scope
-var map;
+/* var map;
 //function to instantiate the Leaflet map
 function createMap(){
     //create the map
@@ -34,29 +140,46 @@ function onEachFeature(feature, layer) {
         layer.bindPopup(popupContent);
     };
 };
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data){
+    var attribute = "Tour_2015";
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
 
-//function to retrieve the data and place it on the map
-function getData(map){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {onEachFeature: onEachFeature,
+      pointToLayer: function (feature, latlng) {
+              //Step 5: For each feature, determine its value for the selected attribute
+              var attValue = Number(feature.properties[attribute]);
+
+              //examine the attribute value to check that it is correct
+              console.log(feature.properties, attValue);
+
+              //create circle markers
+              return L.circleMarker(latlng, geojsonMarkerOptions);
+          }
+      }).addTo(map);
+  };
+
+//Step 2: Import GeoJSON data
+function getData(){
     //load the data
     fetch("data/touristnumbers.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-          var geojsonMarkerOptions = {
-              radius: 8,
-              fillColor: "#ff7800",
-              color: "#000",
-              weight: 1,
-              opacity: 1,
-              fillOpacity: 0.8
-          };
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json, {
-                onEachFeature: onEachFeature,pointToLayer: function (feature, latlng){
-        						return L.circleMarker(latlng, geojsonMarkerOptions);
-        				}
-            }).addTo(map);
+            //call function to create proportional symbols
+            createPropSymbols(json);
         })
 };
+
 document.addEventListener('DOMContentLoaded',createMap)
+*/
